4689b791c33871303556025ab9db370031e0e3ae
heejingit <a_danby@naver.com>
2022-05-12 21:58:17 -0400

    [UserPage] Table added

--- src/components/board/UserBoard.js
+++ src/components/board/UserBoard.js
@@ -1,8 +1,105 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import AuthService from '../auth/service/AuthService';
+import ResponsiveAppBar from './nav/ReponsiveAppBar';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
 const UserBoard = () => {
+    const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const columns = [
+      { id: 1, label: 'Title', minWidth: 300 },
+      { id: 2, label: 'Description', minWidth: 300 },
+      { id: 3, label: 'Created at', minWidth: 100 },
+      { id: 4, label: 'Process', minWidth: 100 },
+      { id: 5, label: 'Rate', minWidth: 50 }
+  ];
+  const rows = []; // real data will be added.
+
+  const handleChangePage = (e, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = e => {
+    setRowsPerPage(e.target.value);
+    setPage(0);
+  };
+    if (!AuthService.isAuthenticated()) return <Navigate to="/signin" replace />;
+    const user = AuthService.getUser();
+    console.log(user);
     return (
-        <h1>hi</h1>
+        <>
+            <ResponsiveAppBar menuList={["Create"]} />
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: '100vh' }}
+            >
+            <Container maxWidth={false}>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 1000 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  <b>{column.label}</b>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+            </Container>    
+        </Grid>
+        </>
     );
 }
 
